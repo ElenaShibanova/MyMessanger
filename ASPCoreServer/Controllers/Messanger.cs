ff099d8ab7dc5b84b9033c0c70e40db11389b678
Елена Шибанова <elenashibanova@inbox.ru>
2022-07-14 15:57:04 +0300

    Добавлен сервер

---
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMessanger;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messanger : ControllerBase
+    {
+        static List<Message> ListOfMessage = new List<Message>();
+        // GET api/<Messanger>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string OutputString = "Not found";
+            if ((id < ListOfMessage.Count) && (id >= 0)) {
+                OutputString =JsonConvert.SerializeObject(ListOfMessage[id]);
+            }
+            Console.WriteLine(String.Format("Запрошено сообщение № {0} : {1}", id, OutputString));
+            return OutputString;
+        }
+
+        // POST api/<Messanger>
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] Message msg)
+        {
+            if (msg == null) {
+                return BadRequest();
+            }
+            ListOfMessage.Add(msg);
+            Console.WriteLine(String.Format("Всего сообщений: {0} Посланное сообщение: {1}", ListOfMessage.Count, msg));
+            return new OkResult();
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger
 {
@@ -6,9 +7,14 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2022-07-13T11:34:20.1686757Z"}
+            //RusAl < 13.07.2022 11:34:20 >:Privet
+        
         }
     }
 }
